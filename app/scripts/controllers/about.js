4daf183aa712df92c0c5e0a52d97db202c03b2c0
Ben <ben.f.linn@gmail.com>
2015-10-13 09:55:34 -0500

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name habitmanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the habitmanApp
+ */
+angular.module('habitmanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
