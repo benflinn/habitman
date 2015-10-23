835907528b3be90de3d4c1313c5eceb91cc7519a
Ben <ben.f.linn@gmail.com>
2015-10-22 20:48:57 -0500

    added service to remember the stats if you visit different views

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,152 +11,149 @@ angular.module('habitmanApp')
     .controller('MainCtrl', function($scope, sharedProperties, userStats) {
         //grabbing titles from the service
         var titles = sharedProperties.getTitles();
-        //setting initial vars
 
-        var sleep = 20,
-            exercise = 20,
-            diet = 20,
-            goals = 20,
-            determination = 30;
-        var stress = 0;
-        var sleepSkill = 20,
-            exerSkill = 20,
-            dietSkill = 20,
-            goalSkill = 20,
-            detSkill = 20;
-        var prod = 20;
-        var joblevel = 0;
-        var wage = 7.5;
-        //defining the vars for the scope
-        $scope.habitpower = 10;
-        $scope.jobtitle = titles[joblevel];
-        $scope.age = 14;
-        $scope.weeklyHours = 40;
-        $scope.hourlyWage = wage;
-        $scope.lifeSavings = 1;
+        //getting userData
+        var user = userStats.getStats();
+
+        //show initial stats
+        $scope.habitpower = user.habitpower;
+        $scope.jobtitle = titles[user.joblevel];
+        $scope.hourlyWage = user.wage;
+        $scope.lifeSavings = user.lifeSavings;
+        $scope.age = user.age;
+
         //functions to give values to the progress bars
         $scope.prodStatus = function() {
-            if (prod > 100) {
-                prod = 0;
-                joblevel++;
-                $scope.jobtitle = titles[joblevel];
-                wage = Math.round(100 * (wage + wage * .05)) / 100;
-                $scope.hourlyWage = wage;
-                stress++;
+            if (user.prod > 100) {
+                user.prod = 0;
+                user.joblevel++;
+                $scope.jobtitle = titles[user.joblevel];
+                user.wage = Math.round(100 * (user.wage + user.wage * .05)) / 100;
+                $scope.hourlyWage = user.wage;
+                user.stress++;
             }
-            if (prod < 0) {
-                prod = 0;
+            if (user.prod < 0) {
+                user.prod = 0;
             }
-            return "width: " + prod + "%;";
+            return "width: " + user.prod + "%;";
         };
         $scope.sleepStatus = function() {
-            if (sleep > 100) {
-                sleep = 100;
+            if (user.sleep > 100) {
+                user.sleep = 100;
             }
-            if (sleep < 0) {
-                sleep = 0;
+            if (user.sleep < 0) {
+                user.sleep = 0;
             }
-            if (sleep > 50) {
+            if (user.sleep > 50) {
                 $scope.sleepImage = 'sleep.png';
             } else {
                 $scope.sleepImage = 'nosleep.png';
             }
-            return "width: " + sleep + "%;";
+            return "width: " + user.sleep + "%;";
         };
         $scope.exerStatus = function() {
-            if (exercise > 100) {
-                exercise = 100;
+            if (user.exercise > 100) {
+                user.exercise = 100;
             }
-            if (exercise < 0) {
-                exercise = 0;
+            if (user.exercise < 0) {
+                user.exercise = 0;
             }
-            if (exercise > 50) {
+            if (user.exercise > 50) {
                 $scope.exerImage = 'exer.png';
             } else {
                 $scope.exerImage = 'noexer.png';
             }
 
-            return "width: " + exercise + "%;";
+            return "width: " + user.exercise + "%;";
         };
         $scope.dietStatus = function() {
-            if (diet > 100) {
-                diet = 100;
+            if (user.diet > 100) {
+                user.diet = 100;
             }
-            if (diet < 0) {
-                diet = 0;
+            if (user.diet < 0) {
+                user.diet = 0;
             }
-            if (diet > 50) {
+            if (user.diet > 50) {
                 $scope.dietImage = 'diet.png';
             } else {
                 $scope.dietImage = 'nodiet.png';
             }
 
-            return "width: " + diet + "%;";
+            return "width: " + user.diet + "%;";
         };
         $scope.goalStatus = function() {
-            if (goals > 100) {
-                goals = 100;
+            if (user.goals > 100) {
+                user.goals = 100;
             }
-            if (goals < 0) {
-                goals = 0;
+            if (user.goals < 0) {
+                user.goals = 0;
             }
-            if (goals > 50) {
+            if (user.goals > 50) {
                 $scope.goalImage = 'goals.png';
             } else {
                 $scope.goalImage = 'nogoals.png';
             }
-            return "width: " + goals + "%;";
+            return "width: " + user.goals + "%;";
         };
         $scope.detStatus = function() {
-            if (determination > 100) {
-                determination = 100;
+            if (user.determination > 100) {
+                user.determination = 100;
             }
-            if (determination < 0) {
-                determination = 0;
+            if (user.determination < 0) {
+                user.determination = 0;
             }
-            if (determination > 50) {
+            if (user.determination > 50) {
                 $scope.detImage = 'determine.png';
             } else {
                 $scope.detImage = 'nodetermine.png';
             }
-            return "width: " + determination + "%;";
+            return "width: " + user.determination + "%;";
         };
         //buttons to raise the habit progress bars
         $scope.nap = function() {
-            sleep += sleepSkill - stress * $scope.weeklyHours / 100;
+            user.sleep += user.sleepSkill - user.stress * user.weeklyHours / 100;
         }
         $scope.walk = function() {
-            exercise += exerSkill - stress * $scope.weeklyHours / 100;
+            user.exercise += user.exerSkill - user.stress * user.weeklyHours / 100;
         }
         $scope.snack = function() {
-            diet += dietSkill - stress * $scope.weeklyHours / 100;
+            user.diet += user.dietSkill - user.stress * user.weeklyHours / 100;
         }
         $scope.plan = function() {
-            goals += goalSkill - stress * $scope.weeklyHours / 100;
+            user.goals += user.goalSkill - user.stress * user.weeklyHours / 100;
         }
         $scope.focus = function() {
-            determination += detSkill - stress * $scope.weeklyHours / 100;
+            user.determination += user.detSkill - user.stress * user.weeklyHours / 100;
         }
         $scope.rotateHours = function() {
-                $scope.weeklyHours += 20;
-                if ($scope.weeklyHours > 110) {
-                    $scope.weeklyHours = 20;
-                }
+            user.weeklyHours += 20;
+            if (user.weeklyHours > 110) {
+                user.weeklyHours = 20;
             }
-            //function for each half second update
+            $scope.weeklyHours = user.weeklyHours;
+        }
+
+        //function for each half second update
         var oneSecond = function() {
-                $scope.lifeSavings = Math.round($scope.lifeSavings + $scope.hourlyWage * $scope.weeklyHours / 8.3);
-                $scope.age = Math.round(10000 * ($scope.age + .0024)) / 10000;
-                $scope.sleepPercent = $scope.sleepPercent + 1;
+
+                //add on money and update in DOM
+                user.lifeSavings = Math.round(user.lifeSavings + user.hourlyWage * user.weeklyHours / 8.3);
+                $scope.lifeSavings = user.lifeSavings;
+
+                //add on age and update in DOM
+                user.age = Math.round(10000 * (user.age + .0024)) / 10000;
+                $scope.age = user.age;
+
                 //add variance & add stress
-                sleep = sleep + 2 - 4 * Math.random() - stress * $scope.weeklyHours / 100 + sleepSkill / 20;
-                determination = determination + 2 - 4 * Math.random() - stress * $scope.weeklyHours / 100 + detSkill / 20;
-                diet = diet + 2 - 4 * Math.random() - stress * $scope.weeklyHours / 100 + dietSkill / 20;
-                exercise = exercise + 2 - 4 * Math.random() - stress * $scope.weeklyHours / 100 + exerSkill / 20;
-                goals = goals + 2 - 4 * Math.random() - stress * $scope.weeklyHours / 100 + goalSkill / 20;
+                user.sleep = user.sleep + 2 - 4 * Math.random() - user.stress * user.weeklyHours / 100 + user.sleepSkill / 20;
+                user.determination = user.determination + 2 - 4 * Math.random() - user.stress * user.weeklyHours / 100 + user.detSkill / 20;
+                user.diet = user.diet + 2 - 4 * Math.random() - user.stress * user.weeklyHours / 100 + user.dietSkill / 20;
+                user.exercise = user.exercise + 2 - 4 * Math.random() - user.stress * user.weeklyHours / 100 + user.exerSkill / 20;
+                user.goals = user.goals + 2 - 4 * Math.random() - user.stress * user.weeklyHours / 100 + user.goalSkill / 20;
                 //calculate productivity
-                prod = prod + sleep / 100 + diet / 100 + exercise / 100 + determination / 100 + goals / 100 - 2.5;
+                user.prod = user.prod + user.sleep / 100 + user.diet / 100 + user.exercise / 100 + user.determination / 100 + user.goals / 100 - 2.5;
                 $scope.$apply();
+                console.log(userStats.getStats());
             }
             //starting the update cycle
         setInterval(function() {
--- app/scripts/services/userStats.js
+++ app/scripts/services/userStats.js
@@ -1,13 +1,33 @@
 app.service('userStats', function() {
-        return {
+
+    var user = {
+        sleep: 20,
+        exercise: 20,
+        diet: 20,
+        goals: 20,
+        determination: 30,
+        stress: 0,
+        sleepSkill: 20,
+        exerSkill: 20,
+        dietSkill: 20,
+        goalSkill: 20,
+        detSkill: 20,
+        prod: 20,
+        joblevel: 0,
+        wage: 7.5,
+        habitpower: 10,
+        age: 14,
+        weeklyHours: 40,
+        hourlyWage: 7.5,
+        lifeSavings: 1
+    }
+
+    return {
         getStats: function() {
-            var stats = {
-                
-            }
-            return stats;
+            return user;
         },
         setStats: function(value) {
             stats = value;
         },
     }
-    });
+});
